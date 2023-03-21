441d8dbf9d95929f929b582e2d0292ff3d5fc059
unknown <sharonkessio6@gmail.com>
2023-03-21 11:57:54 +0300

    sessions and reg controllers setup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,19 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
+  respond_to :json
+
+  private
+   def respond_with(resource, options={})
+    if resource.persisted?
+      render json: {
+        status: {
+          code: 200, message: 'Signup Successfully',
+          data: resource
+        }, status: :ok
+      }
+    else
+      render json: {
+        status: { message: 'User not created Successfully',
+        error: resource.error.full_messages}, status: :unprocessable_entity
+      }
+    end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,37 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  respond_with(resource, options={})
+  render json: {
+    status: {
+      code: 200,
+      message: 'User Signed In Successfully',
+      data: current_user
+    }, status: :ok
+  }
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
 
-  # protected
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last, ENV.fetch('DEVISE_JWT_SECRET')).first
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+      current_user = User.find(jwt_payload['sub'])
+
+      if current_user
+        render json: {
+          status: 200,
+          message: 'Signed Out Successfully'
+        }, status: ok
+
+        else 
+          render json: {
+            status: 401,
+            message: 'User has no active session'
+          }, status: :unauthorized
+        end
+    end
+  end
+  
 end
