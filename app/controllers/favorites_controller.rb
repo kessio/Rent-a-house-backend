859e3af9ba557f3e9edd7d55ada4736d1e446558
ecekpo <ememekpo@yahoo.com>
2023-03-24 11:21:18 +0100

    add method create in favorite controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,8 @@
 class FavoritesController < ApplicationController
+    
+    def create
+        @house = House.find(params[:house_id])
+        current_user.favorite_houses << @house
+        redirect_to @house, notice: "House has been added to your favorites"
+      end
 end
