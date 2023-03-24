98c1c9dfa9fc30fba6900197129357b23c02973c
ecekpo <ememekpo@yahoo.com>
2023-03-24 11:22:33 +0100

    add destroy method for favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,14 @@
 class FavoritesController < ApplicationController
-    
+
     def create
         @house = House.find(params[:house_id])
         current_user.favorite_houses << @house
         redirect_to @house, notice: "House has been added to your favorites"
+      end
+
+      def destroy
+        @favorite = current_user.favorites.find_by(house_id: params[:id])
+        @favorite.destroy
+        redirect_to root_path, notice: "House has been removed from your favorites"
       end
 end
