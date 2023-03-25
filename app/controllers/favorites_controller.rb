01796b456e0dc7069eff9765d742e29d79e8e6c8
ecekpo <ememekpo@yahoo.com>
2023-03-25 15:51:01 +0100

    add to favorite method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,15 @@ class FavoritesController < ApplicationController
 
     before_action :authenticate_user!
 
-    def index
-        @favorites = current_user.favorites
-        render json: @favorites, each_serializer: FavoriteSerializer
+    def add_to_favorites
+      favorite = Favorite.new(user_id: current_user.id, house_id: params[:house_id])
+      if favorite.save
+        render json: favorite, status: :created
+      else
+        render json: favorite.errors, status: :unprocessable_entity
       end
-      
+    end
+    
     def create
         @house = House.find(params[:house_id])
         current_user.favorite_houses << @house
