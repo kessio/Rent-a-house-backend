33ea2c328d41c5fa2653ecd04a7a4e5fb9f9dc0b
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-25 14:39:25 +0000

    check if image is blank

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,4 +5,5 @@ class House < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :image, presence: true
 end
