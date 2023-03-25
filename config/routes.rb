66f7e22f87c3381772417dded95e67e33d204b4d
kessio <sharonkessio6@gmail.com>
2023-03-25 18:25:09 +0300

    add routes for show action

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :users, only: [:index] 
-      resources :houses, only: [:index, :create, :destroy]
+      resources :houses, only: [:index, :create, :destroy, :show]
       end
     end
   
