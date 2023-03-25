305c54345f169e5628212c591305aaca543f4bbb
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-25 14:46:09 +0000

    fix linters error

--- app/models/house.rb
+++ app/models/house.rb
@@ -7,7 +7,7 @@ class House < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image, presence: true
   validates :image, content_type: { in: %w[image/jpeg image/png image/gif image/webp],
-    message: "must be a valid image format" }
+                                    message: 'must be a valid image format' }
   validates :image, size: { less_than: 5.megabytes,
-    message: "should be less than 5MB" }
+                            message: 'should be less than 5MB' }
 end
