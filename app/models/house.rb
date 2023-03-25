dd15fbb9c746217902201b77968daa0c1903c90e
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-25 14:51:23 +0000

    update validations

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,9 +2,15 @@ class House < ApplicationRecord
   has_many :favorites
   has_many :users, through: :favorites
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: { message: "can't be blank" }, numericality:
+                              { greater_than_or_equal_to: 0, message:
+                              "must be a positive number" }
+  validates :description, presence: { message: "can't be blank" }, length:
+                                    { minimum: 10, maximum: 500, message:
+                                    "must be between 10 and 500 characters" }
+  validates :price, presence: { message: "can't be blank" }, numericality:
+                              { greater_than_or_equal_to: 0, message:
+                              "must be a positive number" }
   validates :image, presence: true
   validates :image, content_type: { in: %w[image/jpeg image/png image/gif image/webp],
                                     message: 'must be a valid image format' }
