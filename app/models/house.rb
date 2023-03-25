74ee5ffb361914e4b4f0aad4d7376c82d1e5f80c
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-25 14:40:57 +0000

    validating image type

--- app/models/house.rb
+++ app/models/house.rb
@@ -6,4 +6,6 @@ class House < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image, presence: true
+  validates :image, content_type: { in: %w[image/jpeg image/png image/gif image/webp],
+    message: "must be a valid image format" }
 end
