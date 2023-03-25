07433bf073601de464b3f9536489fd614da177cc
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-25 14:53:43 +0000

    fix linters error

--- app/models/house.rb
+++ app/models/house.rb
@@ -4,13 +4,13 @@ class House < ApplicationRecord
 
   validates :price, presence: { message: "can't be blank" }, numericality:
                               { greater_than_or_equal_to: 0, message:
-                              "must be a positive number" }
+                              'must be a positive number' }
   validates :description, presence: { message: "can't be blank" }, length:
                                     { minimum: 10, maximum: 500, message:
-                                    "must be between 10 and 500 characters" }
+                                    'must be between 10 and 500 characters' }
   validates :price, presence: { message: "can't be blank" }, numericality:
                               { greater_than_or_equal_to: 0, message:
-                              "must be a positive number" }
+                              'must be a positive number' }
   validates :image, presence: true
   validates :image, content_type: { in: %w[image/jpeg image/png image/gif image/webp],
                                     message: 'must be a valid image format' }
