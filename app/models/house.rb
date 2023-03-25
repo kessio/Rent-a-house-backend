832c9d326e23ad4b153a5ee0d2ef6e8604a5382a
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-25 14:38:03 +0000

    validate house description

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,4 +3,5 @@ class House < ApplicationRecord
   has_many :users, through: :favorites
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
 end
