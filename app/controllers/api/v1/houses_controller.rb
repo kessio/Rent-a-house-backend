3c01dd47d03bf5a5eac5858af3a3790d587cdc9c
kessio <sharonkessio6@gmail.com>
2023-03-25 17:21:58 +0300

    fix rubocop linter erros

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -6,27 +6,30 @@ class Api::V1::HousesController < ApplicationController
     render json: @houses, status: :ok
   end
 
-    def create
-      if current_user
-        house = House.new(house_params)
+  def create
+    if current_user
+      house = House.new(house_params)
 
-        if house.save
-          if house.image.attached?
-          render json: { status: 'success', message: 'House created successfully', data: HouseSerializer.new(house) }, status: :created
-          else
-            render json: { status: 'error', message: 'Failed to attach image to house', errors: 'Image is not attached' }, status: :unprocessable_entity
-          end
+      if house.save
+        if house.image.attached?
+          render json: { status: 'success', message: 'House created successfully', data: HouseSerializer.new(house) },
+                 status: :created
         else
-          render json: { status: 'error', message: 'Failed to create house', errors: house.errors.full_messages }, status: :unprocessable_entity
+          render json: { status: 'error', message: 'Failed to attach image to house', errors: 'Image is not attached' },
+                 status: :unprocessable_entity
         end
       else
-        render json: { errors: 'You are not authorized to create a house.' }, status: :unauthorized
+        render json: { status: 'error', message: 'Failed to create house', errors: house.errors.full_messages },
+               status: :unprocessable_entity
       end
-      end
-    
-      private
-    
-      def house_params
-        params.require(:house).permit(:title, :description, :price, :image).merge(user_id: current_user.id)
-      end
+    else
+      render json: { errors: 'You are not authorized to create a house.' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:title, :description, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,11 +1,10 @@
 class House < ApplicationRecord
   has_one_attached :image
-  
+
   has_many :favorites
   has_many :users, through: :favorites
 
   def image_url
-      Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
   end
-
 end
