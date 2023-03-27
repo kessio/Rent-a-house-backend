e1978daa8fa76347a5f5fdea7172bbf490f2f3d1
ecekpo <ememekpo@yahoo.com>
2023-03-27 11:44:19 +0100

    add favorite serializer in favorite controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,11 +1,12 @@
-class FavoritesController < ApplicationController
+class Api::V1::FavoritesController < ApplicationController
 
     before_action :authenticate_user!
 
     def create
       favorite = Favorite.new(user_id: current_user.id, house_id: params[:house_id])
       if favorite.save
-        render json: favorite, status: :created
+        render json: { status: 'success', message: 'Favorite created successfully', data: FavoriteSerializer.new(favorite) },
+                 status: :created
       else
         render json: favorite.errors, status: :unprocessable_entity
       end
