959b131966661a50feddb6070d26c7dc9fd34cd2
ecekpo <ememekpo@yahoo.com>
2023-04-04 12:16:06 +0100

    fix linter errors

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -9,7 +9,10 @@ RSpec.describe House, type: :model do
     it { should validate_presence_of(:title).with_message("can't be blank") }
     it { should validate_presence_of(:description).with_message("can't be blank") }
     it {
-      should validate_length_of(:description).is_at_least(10).is_at_most(500).with_message'must be between 10 and 500 characters'
+      should validate_length_of(:description)
+        .is_at_least(10)
+        .is_at_most(500)
+        .with_message 'must be between 10 and 500 characters'
     }
     it { should validate_presence_of(:price).with_message("can't be blank") }
     it {
